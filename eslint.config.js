e5aa598412e07ff79867563c9b5804c0e27211ab
Vitor <vitor@gmail.com>
2026-02-15 01:32:07 -0300

    chore: add shadcn

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,26 @@
-import js from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
-import globals from 'globals'
+import globals from "globals";
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
       "simple-import-sort": simpleImportSort,
     },
     rules: {
@@ -28,12 +28,13 @@ export default [
       ...reactHooks.configs.recommended.rules,
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/prop-types": "off",
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
   },
   eslintConfigPrettier,
-]
+];
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
 
 function App() {
-
   return (
-    <h1 className="text-red-700">teste</h1>
-  )
+    <>
+      <h1 className="text-red-700">teste</h1>
+      <Button>Teste</Button>
+    </>
+  );
 }
 
-export default App
+export default App;
