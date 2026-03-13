502e048734612f49b1b3e110156d75f7c3a95385
Vitor <Vitor@email.com>
2026-03-13 17:36:05 -0300

    teste

---
+++ src/components/ui/calendar-pick.jsx
@@ -0,0 +1,44 @@
+import {useState} from "react"
+
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import { Field, FieldLabel } from "@/components/ui/field"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+export function DatePickerSimple({ label}) {
+  const [open, setOpen] = useState(false)
+  const [date, setDate]  = useState(undefined)
+
+  return (
+    <Field className="mx-auto w-44">
+      <FieldLabel htmlFor="date">{label}</FieldLabel>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date"
+            className="justify-start font-normal"
+          >
+            {date ? date.toLocaleDateString() : "Selecione a Data"}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            defaultMonth={date}
+            captionLayout="dropdown"
+            onSelect={(date) => {
+              setDate(date)
+              setOpen(false)
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+    </Field>
+  )
+}
